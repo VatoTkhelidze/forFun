9e5061667c5fbb0b6c6c6ec68b38edf0fe8d8bf2
VatoTkhelidze <vatotxelidze@gmail.com>
2023-09-04 23:38:37 +0400

    updated functionality on boys photos

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const datucha = document.getElementById('brother_div1');
-const brotherBox = document.querySelectorAll('.brotherBox');
+const brotherBox = document.querySelectorAll(".brotherBox");
+const mainBox = document.querySelector(".main_div");
+const boxAlignment = document.querySelector(".main_container");
 
 const deleteOtherPics = (clickedElement) => {
-  for(let i=0; i < clickedElement.length; i++){
-    clickedElement[i].addEventListener('click', ()=> {
-      for(let d = 0; d < clickedElement.length; d++){
-        if(d !== i){
-          clickedElement[d].style.display = 'none';
+  for (let i = 0; i < clickedElement.length; i++) {
+    clickedElement[i].addEventListener("click", () => {
+      clickedElement[i].style.transition = "all 0.4s ease-in-out";
+      boxAlignment.style.transition = "all 0.4s ease-in-out";
+      boxAlignment.style.transform = "translate(0, -39%)";
+      boxAlignment.style.minHeight = "100vh";
+      mainBox.style.width = '100%';
+      clickedElement[i].style.transform = "translateX(-105px)";
+      for (let d = 0; d < clickedElement.length; d++) {
+        if (d !== i) {
+          clickedElement[d].style.display = "none";
         }
       }
-      // clickedElement[i].style.display = 'none');
-      console.log('zaza')
+      console.log(clickedElement[i]);
     });
   }
-}
+};
 
 deleteOtherPics(brotherBox);
