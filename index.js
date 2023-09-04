c5feb08e19ed825ff9f65d3ef3001433ee1f8225
VatoTkhelidze <vatotxelidze@gmail.com>
2023-09-05 00:43:06 +0400

    created name spans with transition for boys :))

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const brotherBox = document.querySelectorAll(".brotherBox");
 const mainBox = document.querySelector(".main_div");
 const boxAlignment = document.querySelector(".main_container");
+const removeGap = document.getElementById("first_line");
+const broText = document.querySelectorAll(".brotherText");
+const bondMeister = document.getElementById("bondMaster");
 
 const deleteOtherPics = (clickedElement) => {
   for (let i = 0; i < clickedElement.length; i++) {
     clickedElement[i].addEventListener("click", () => {
+      // broText[i].style.display = "block";
+      broText[i].style.right = "-30px";
+      broText[i].style.color = "white";
+      broText[i].style.transition = "all 2.4s ease-in-out";
+      bondMeister.style.right = "-70px";
+      removeGap.style.gap = "0";
       clickedElement[i].style.transition = "all 0.4s ease-in-out";
       boxAlignment.style.transition = "all 0.4s ease-in-out";
       boxAlignment.style.transform = "translate(0, -39%)";
